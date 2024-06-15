8bb54d835627ff4749d47ce5ed8e954e94eb9c6a
antho <anthonywollena@gmail.com>
2024-06-15 19:45:52 -0300

    Adicionado protótipo do login

--- app/src/main/java/com/example/repfatecdm/MainActivity.kt
+++ app/src/main/java/com/example/repfatecdm/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.example.repfatecdm
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val usuario = findViewById<EditText>(R.id.txtUsuario)
+        val senha = findViewById<EditText>(R.id.txtSenha)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            val user = usuario.text.toString()
+            val pass = senha.text.toString()
+
+            if (user == "admin" && pass == "1234") {
+                Toast.makeText(this, "Login realizado", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Login falhou", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
